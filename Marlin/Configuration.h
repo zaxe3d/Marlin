9588d91a93951d48e3f7fd1eb115ee80d87cd24e
Gokhan Onis <gokhan.onis@zaxe.com>
2021-09-29 15:26:59 +0300

    config update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -748,7 +748,7 @@
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 80, 400, 415}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 415}
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
