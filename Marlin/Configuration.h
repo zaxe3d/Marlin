ac22c0567ebfe9292067c19a2138e0ef96b29f07
Elsan2020 <71882913+Elsan2020@users.noreply.github.com>
2021-10-18 10:59:28 +0300

    Filament temperatures updated.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1513,13 +1513,13 @@
 
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 180
-#define PREHEAT_1_TEMP_BED     70
+#define PREHEAT_1_TEMP_HOTEND 210 //180
+#define PREHEAT_1_TEMP_BED     60 //70
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    110
+#define PREHEAT_2_TEMP_HOTEND 250 //240
+#define PREHEAT_2_TEMP_BED    100 //110
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_3_LABEL       "PETG"
