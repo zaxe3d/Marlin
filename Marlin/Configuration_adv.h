0672e3c2205b43c15ffc2e494c41a12f871f94de
Gokhan Onis <gokhanonis@gmail.com>
2022-04-08 12:31:01 +0300

    xy rsense updated.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2274,7 +2274,7 @@
     #define X_CURRENT       1200        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16    // 0..256
-    #define X_RSENSE          0.22//0.11
+    #define X_RSENSE         0.10
     #define X_CHAIN_POS      -1    // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
   #endif
 
@@ -2290,7 +2290,7 @@
     #define Y_CURRENT       1200
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
-    #define Y_RSENSE          0.22//0.11
+    #define Y_RSENSE         0.10
     #define Y_CHAIN_POS      -1
   #endif
 
