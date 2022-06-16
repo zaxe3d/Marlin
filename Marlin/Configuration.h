d19f2c10bc10a5f60521e210d170323d30862f9f
Gokhan Onis <gokhanonis@gmail.com>
2022-06-16 15:37:55 +0300

    Alican's configuration update.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1133,12 +1133,12 @@
 #define Y_BED_SIZE 245
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -6
-#define Y_MIN_POS 0
+#define X_MIN_POS -2 //Alican //-6
+#define Y_MIN_POS -0.5 //Alican //0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 250 //200 //Elsan
+#define Z_MAX_POS 255 // Alican //250 //200 //Elsan
 
 /**
  * Software Endstops
